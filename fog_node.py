8929192b09d7981987b2fbc4c441cd4b0f365c12
charm <hdegdeg24@gmail.com>
2024-11-15 22:07:34 +0100

    start generate credential for user

--- fog_node.py
+++ fog_node.py
@@ -13,6 +13,8 @@ import base64
 import sqlite3
 import charm.toolbox.symcrypto
 import pickle
+import random
+from typing import List, Tuple
 
 class FogNode:
     def __init__(self, group: str, scheme=MaabeRW15):
@@ -56,9 +58,18 @@ class FogNode:
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 obj_id INTEGER NOT NULL,
                 action_name TEXT UNIQUE NOT NULL,
+                rvo TEXT UNIQUE NOT NULL,
                 key_value TEXT NOT NULL
             )
             ''')
+
+            self.cursor_fog.execute('''
+            CREATE TABLE IF NOT EXISTS obj_primes_table (
+                obj_id INTEGER PRIMARY KEY ,
+                prime TEXT UNIQUE NOT NULL,
+            )
+            ''')
+            
             self.conn_with_bdd_fog.commit()
             print("Table 'obj_abe_keys_table' initialisée.")
         except sqlite3.Error as e:
@@ -91,11 +102,15 @@ class FogNode:
 
 
     def generate_token_for_action(self,actions, id_obj,access_policy):
+        PRIME = randprime(10**100, 10**101)
+        result_dict={}
 
         # Parcourir la liste des actions
         for action in actions:
             # Génération du message aléatoire
             message1 = self.group.random(GT)
+            # Générer une valeur aléatoire de 256 bits
+            RVO = random.getrandbits(256)
             
             # Chiffrement du message avec MA-ABE
             cipher_text = self.maabe.encrypt(self.public_parameters, self.public_keys, message1, access_policy)
@@ -105,13 +120,90 @@ class FogNode:
             print("have ben seralized")
             
             # Stocker le chemin du fichier dans la base de données
-            print("id_obj:", id_obj, "action:", action)
-            self.cursor_fog.execute('INSERT INTO obj_abe_keys_table (obj_id, action_name, key_value) VALUES (?, ?, ?)', 
-                        (id_obj, f"{id_obj}_{action}", serialized_message))
+            self.cursor_fog.execute(
+            'INSERT INTO obj_abe_keys_table (obj_id, action_name, key_value, rvo) VALUES (?, ?, ?, ?)',
+            (id_obj, f"{id_obj}_{action}", serialized_message, RVO)
+            )
+
+            # Ajouter au dictionnaire le tuple (message1, RVO)
+            self.cursor_fog.execute(
+                'INSERT INTO obj_abe_keys_table (obj_id, action_name, key_value, rvo) VALUES (?, ?, ?, ?)',
+                (id_obj, f"{id_obj}_{action}", serialized_message, RVO)
+            )
+
+            # Ajouter au dictionnaire le tuple (message1, RVO)
+            result_dict[action] = (message1, RVO)
+
+        self.cursor_fog.execute(
+                'INSERT INTO obj_primes_table (obj_id,prime) VALUES (?, ?)',
+                (id_obj, PRIME)
+            )
         
         # Commit des changements dans la base de données
         self.conn_with_bdd_fog.commit()
 
+        return result_dict, PRIME
+
+#-----------------------------------------------------------CREATE SHARE FOR USER----------------------------------------------------    
+
+    def generate_coefficients(secret: int, fixed_share: Tuple[int, int], PRIME) -> List[int]:
+
+        """
+        Génère les coefficients du polynôme de Shamir en fixant une part spécifique.
+        """
+        x_fixed, y_fixed = fixed_share
+        # Le terme constant est le secret
+        c0 = secret
+        # Le coefficient suivant est calculé pour que f(x_fixed) = y_fixed
+        c1 = (y_fixed - c0) % PRIME
+        return PRIME,[c0, c1]
+
+    def create_shares(self,secret: int, fixed_share: Tuple[int, int], total_shares: int) -> List[Tuple[int, int]]:
+        """
+        Génère les parts du secret en fixant une part et en générant les autres dynamiquement.
+        """
+        coefficients = self.generate_coefficients(secret, fixed_share)
+        shares = [fixed_share]
+        for x in range(1, total_shares + 1):
+            if x == fixed_share[0]:
+                continue  # Sauter la part fixe
+            # Évaluer le polynôme à x
+            y = sum(coeff * (x**exp) for exp, coeff in enumerate(coefficients)) % PRIME
+            shares.append((x, y))
+        return shares
+
+    def get_credential_of_action(self, action, id_obj):
+    
+        # Récupération du chemin de fichier depuis la base de données
+        self.cursor_fog.execute('SELECT * FROM obj_abe_keys_table WHERE action_name = ?', (action,)) 
+        rows = self.cursor_fog.fetchone()  
+
+        if rows:
+            for row in rows:
+                Token = row[3]
+                RVO =  row[4]
+        else:
+            print("Aucun résultat trouvé pour l'action spécifiée.")
+
+        self.cursor_fog.execute('SELECT * FROM obj_primes_table WHERE id_obj = ?', (id_obj,)) 
+        rows = self.cursor_fog.fetchone()  
+
+        if rows:
+            for row in rows:
+                PRIME = row[2]
+        else:
+            print("Aucun résultat trouvé pour l'action spécifiée.")
+        
+        return Token,RVO,PRIME
+    
+    
+
+    def generate_credential_for_action(self,action):
+        self.get_token_of_action(action)
+
+        
+        print()
+#------------------------------------------------------------------------------------------------------------------------------------
 def main():
     fog = FogNode('SS512', MaabeRW15)
     print("Nœud Fog initialisé avec succès.")
